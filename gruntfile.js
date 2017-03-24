bae5aa6c3c68c8790d01b65b3fd54ffbcb537092
Nicole Haugen <nicolela@microsoft.com>
2017-03-24 13:14:50 -0500

    added uglify

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,30 @@
 module.exports = function(grunt) {
+
     grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
         mochaTest: {
             src: 'test.js',
             options: {
                 reporter: 'spec'
             }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+            },
+            build: {
+                files: {
+                    'dist/app.min.js' : 'app.js'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('test', 'mochaTest');
 };
